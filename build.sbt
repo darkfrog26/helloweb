9f81d7ffb1f1b89b8796b7ed223a1a9582236d87
Matt Hicks <matt@outr.com>
2015-09-07 13:16:47 -0500

    HelloWorld example

---
+++ build.sbt
@@ -0,0 +1,5 @@
+version := "1.0.0-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+sbtVersion := "0.13.8"
---
+++ src/main/scala/matt/HelloWorld.scala
@@ -0,0 +1,42 @@
+package matt
+
+/**
+ * @author Matt Hicks <matt@outr.com>
+ */
+class HelloWorld extends HTML {
+  val head = children.add(new Head)
+  val body = children.add(new Body)
+
+  val heading = body.children.add(new H1)
+  heading.content := "Hello, World!"
+}
+
+trait HTMLTag
+
+trait Container[Child <: HTMLTag] {
+  object children {
+    def add[T <: Child](child: T): T = {}
+  }
+}
+
+class HTML extends Container[HTMLChild]
+
+trait HTMLChild extends HTMLTag
+
+class Head extends HTMLChild with Container[HeadChild]
+
+class Body extends HTMLChild with Container[BodyChild]
+
+trait HeadChild extends HTMLTag
+
+trait BodyChild extends HTMLTag
+
+class H1 extends BodyChild {
+  val content = new Prop[String]
+}
+
+class Prop[T] {
+  private var value: T = _
+
+  def :=(value: T) = this.value = value
+}
